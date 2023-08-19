ea430de77df0f7732c1ba6d13ee114cf5d36c3f1
vedik <vediktiwari23@gmail.com>
2023-08-19 23:27:18 +0530

    conditionals statements

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int salary = 26600;
+        // if statements
+       /* if (salary >= 1000) {
+            System.out.println("you are doing good");
+            System.out.print(salary = salary+10000);
+        } else {
+            System.out.println("you are niccompoop");
+             */
+
+        }
+    }
+}
