d514c5a0d200c93cb27e13ea3c09544038c64e0f
vedik <vediktiwari23@gmail.com>
2023-08-20 00:08:34 +0530

    conditionals statements

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,21 @@
-public class Main {
-    public static void main(String[] args) {
-        int salary = 26600;
-        // if statements
-       /* if (salary >= 1000) {
-            System.out.println("you are doing good");
-            System.out.print(salary = salary+10000);
-        } else {
-            System.out.println("you are niccompoop");
-             */
+import java.util.Scanner;
+class Main {
+public static void main(String[] args) {
+     Scanner Number = new Scanner(System.in);
+     int number = Number.nextInt();
 
+        // checks if number is greater than 0
+        if (number > 0) {
+        System.out.println("The number is positive.");
         }
-    }
-}
+
+        // execute this block
+        // if number is not greater than 0
+        else {
+        System.out.println("The number is not positive.");
+        }
+
+        System.out.println("Statement outside if...else block");
+        }
+        }
+
